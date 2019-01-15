464d0032c23b72c524b2550ed4915c88f674b0d2
Tristan Jahnke <trjahnke@gmail.com>
2019-01-15 17:03:45 -0600

    heroku test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route("/")
 def hello():
-    return os.environ["APP_SETTINGS"]
+    return os.environ["SECRET_KEY"]
 
 @app.route("/<name>")
 def hello_name(name):
