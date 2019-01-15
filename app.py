2d46bbe646a03d344991d26d99be62ef46950030
Tristan Jahnke <trjahnke@gmail.com>
2019-01-15 14:21:07 -0600

    added in changes to name landing page

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-
 def hello():
-    return "Hello Tristan!"
+    return "Hello World!"
+
+@app.route("/<name>")
+def hello_name(name):
+    return "Hello {}!".format(name)
 
 if __name__ == "__main__":
     app.run()
